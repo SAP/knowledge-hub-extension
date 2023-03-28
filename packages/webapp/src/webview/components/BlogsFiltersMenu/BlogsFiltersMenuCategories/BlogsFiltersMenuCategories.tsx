618b3b26a48afbdd25c26479200dcae5a4f8f4f6
Ouin, Edouard <edouard.ouin@sap.com>
2023-03-28 11:14:39 +0100

    feat: #31 - fix lint issues

--- packages/webapp/src/webview/components/BlogsFiltersMenu/BlogsFiltersMenuCategories/BlogsFiltersMenuCategories.tsx
+++ packages/webapp/src/webview/components/BlogsFiltersMenu/BlogsFiltersMenuCategories/BlogsFiltersMenuCategories.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import type { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
